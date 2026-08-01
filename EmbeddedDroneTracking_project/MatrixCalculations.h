e4f0a4244a5890428f57278836c46127c5971942
Erik Jonsson <erikjonsson9001@gmail.com>
2026-08-01 12:28:49 +0200

    feat: add QR-decomposition and least squares problem solvers

--- EmbeddedDroneTracking_project/MatrixCalculations.h
+++ EmbeddedDroneTracking_project/MatrixCalculations.h
@@ -1,3 +1,6 @@
+//This class performs matrix calculations which mainly focuses on linearly dependence, dot product and decompositions of a matrix
+//in order to solve a least square problem, especially LU/QR where the latter uses Givens rotation matrices. 
+
 #include "MatrixObject.h"
 #include <tuple>
 #include <iostream>
@@ -13,9 +16,9 @@ class MatrixCalculations{
 
 public:
 
-const bool isLinearIndependent(const MatrixObject<T>& A) const{
+const bool isLinearDependent(const MatrixObject<T>& A) const{
 MatrixObject<T> G = A;
-bool isLinIndep = false;
+bool isLinDep = false;
 int num_of_iterations = min(G.getRows(),G.getCols());
 for(int i = 0;i < num_of_iterations;i++){
    T pivot = G(i,i);
@@ -40,15 +43,14 @@ for(int i = 0;i < G.getRows();i++){
       }  
    }
    if(number_of_zeros == G.getCols()){
-      isLinIndep = true;
-      cout << "The matrix has linear dependent columns.";
+      isLinDep = true;
       break;
    }
 }
-if(isLinIndep == false){
+if(isLinDep == false){
   cout << "The matrix is linear independent.";
 }
-return isLinIndep;   
+return isLinDep;   
 }
 
 const T dot_product(const MatrixObject<T>& v1, const MatrixObject<T>& v2) const{
@@ -57,36 +59,6 @@ for(int i = 0;i < v1.getRows();i++){
    summation += v1(i,v1.getCols()-1)*v2(i,v2.getCols()-1);
 }
 return summation;   
-}
-
-const bool HasOrthogonalColumns(const MatrixObject<T>& A,int c1,int c2)const {
-bool is_orth = false;
-MatrixObject<T> v1 = A.getColumnVector(c1);
-MatrixObject<T> v2 = A.getColumnVector(c2);
-T val = dot_product(v1,v2);
-cout << "Dot product: " << val;
-const double eps = 1e-8;
-if(abs(val) < eps){
-  is_orth = true; 
-}
-return is_orth;
-}
-
-const bool isOrthogonal(MatrixObject<T>& A) const{
-bool orth = false;
-int counter = 0;
-for(int j1 = 0;j1 < A.getCols()-1;j1++){
-   for(int j2 = j1+1;j2 < A.getCols();j2++){
-      if(HasOrthogonalColumns(A,j1,j2)){
-        counter++;
-      }
-   }
-}   
-if(counter == A.getCols()){
-   orth = true;
-   cout << "The matrix is orthogonal.";
-}
-return orth;
 }
 
 const MatrixObject<T> generateIdentityMatrix(int r) const{
@@ -209,8 +181,30 @@ for(int i = 0;i < res_vec.getRows();i++){
 
 return sqrt(res);    
 }
-const tuple<MatrixObject<T>,MatrixObject<T>> QRDecomposition_GivensApproach(const MatrixObject<T>& A)const{
 
+const tuple<T,T> GivensParameters(const T& r1, const T& r2) const{ 
+T denum = sqrt(square(r1) + square(r2));
+if(denum == 0){
+  throw runtime_error("The Givens approach diverges."); 
+}
+else{
+   T c = r1/denum;
+   T s = r2/denum;
+   return {c,s};
+}
+}
+
+const MatrixObject<T> GivensMatrix(const MatrixObject<T>& A, const int& annihil_ind, const int& curr_col)const{
+auto [c,s] = GivensParameters(A(annihil_ind-1,curr_col),A(annihil_ind,curr_col)); 
+MatrixObject<T> G = generateIdentityMatrix(A.getRows());
+G(annihil_ind-1,annihil_ind-1) = c;
+G(annihil_ind-1,annihil_ind) =  -s;
+G(annihil_ind, annihil_ind-1) = s;
+G(annihil_ind,annihil_ind) = c;
+return G;   
+}
+
+const tuple<MatrixObject<T>,MatrixObject<T>> QRDecomposition_GivensApproach(const MatrixObject<T>& A)const{
 const double eps = 1e-7;
 MatrixObject<T> Q = generateIdentityMatrix(A.getRows());
 MatrixObject<T> R = A;
@@ -221,14 +215,7 @@ for(int j = 0;j < num_of_iterations;j++){
       if(abs(val) > eps){
         T pivot_val = R(i-1,j);
         T annihilated_val = R(i,j);
-        T denum = sqrt(pivot_val*pivot_val + annihilated_val*annihilated_val);
-        T c = pivot_val/denum;
-        T s = annihilated_val/denum;
-        MatrixObject<T> G = generateIdentityMatrix(A.getRows());
-        G(i-1,i-1) = c;
-        G(i-1,i) = -s;
-        G(i,i-1) = s;
-        G(i,i) = c; 
+        MatrixObject<T> G = GivensMatrix(R,i,j);
         R = G.Transpose()*R;
         Q = Q*G;
       }
@@ -237,33 +224,29 @@ for(int j = 0;j < num_of_iterations;j++){
 return {Q,R};   
 }
 
+const tuple<MatrixObject<T>,MatrixObject<T>> generateTruncatedMatrices(const MatrixObject<T> & A, const MatrixObject<T>& b)const{
+T truncated_rows = min(A.getRows(),A.getCols());
+MatrixObject<T> A_trunc(truncated_rows,A.getCols());
+MatrixObject<T> b_trunc(truncated_rows,b.getCols());
+
+for(int i = 0;i < A_trunc.getRows();i++){
+   for(int j = 0;j < A_trunc.getCols();j++){
+      A_trunc(i,j) = A(i,j);
+   }
+   b_trunc(i,b_trunc.getCols()-1) = b(i,b.getCols()-1);
+}
+
+return {A_trunc, b_trunc};
+}
+
 const MatrixObject<T> LeastSquareSolver_QRDecomposition(const MatrixObject<T>& A, const MatrixObject<T>& b)const{
 const double eps = 1e-7;
-T min_val = min(A.getRows(),A.getCols());
-T differance_val = A.getRows()-min_val;
 auto [Q,R] = QRDecomposition_GivensApproach(A);
-MatrixObject<T> R_tilde(A.getRows()-differance_val,R.getCols());
 MatrixObject<T> b_tilde = Q.Transpose()*b;
-MatrixObject<T> b_filtered(A.getRows()-differance_val,b_tilde.getCols());
-MatrixObject<T> x(b_filtered.getRows(),1);
 
+auto [R_filtered,b_filtered] = generateTruncatedMatrices(R,b_tilde);
 
-for(int i = 0;i < R_tilde.getRows();i++){
-   for(int j = 0; j < R_tilde.getCols();j++){
-      R_tilde(i,j) = R(i,j);
-      }
-   
-   b_filtered(i,b_filtered.getCols()-1) = b_tilde(i,b_tilde.getCols()-1);
-}
-
-for(int i = R_tilde.getRows()-1;i >= 0;i--){
-   T summation = 0.0;
-   for(int j = i+1;j < R_tilde.getCols();j++){
-      summation += R_tilde(i,j)*x(j,x.getCols()-1);
-   }
-   x(i,x.getCols()-1) = (b_filtered(i,b_filtered.getCols()-1) - summation)/R_tilde(i,i);
-}
-return x;
+return BackwardSubstitution(R_filtered,b_filtered);
 }
 
 };
