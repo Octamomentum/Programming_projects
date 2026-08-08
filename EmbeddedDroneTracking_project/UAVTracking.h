3e98e7fdf8b463a379a71bfdbc1032f37e61df2b
Erik Jonsson <erikjonsson9001@gmail.com>
2026-08-08 14:15:35 +0200

    feat: implemented Gauss-Newton strategy

--- EmbeddedDroneTracking_project/UAVTracking.h
+++ EmbeddedDroneTracking_project/UAVTracking.h
@@ -19,80 +19,116 @@ MatrixCalculations<T> matcalc;
 
 public:
 
-MatrixObject<T> distanceMetric(const MatrixObject<T>& L,const MatrixObject<T>& x){
+tuple<MatrixObject<T>,MatrixObject<T>> generateRadar(const Ti& num_of_landmarks, const tuple<T,T>& axis_range, const tuple<Ti,Ti>& seeds){
+
+Ti landmark_seed = get<0>(seeds);
+Ti ref_seed = get<1>(seeds);
+T lb_map = get<0>(axis_range);
+T ub_map = get<1>(axis_range);
+
+mt19937 gen1(landmark_seed);
+mt19937 gen2(ref_seed);
+uniform_real_distribution<T> u_g(lb_map,ub_map);
+
+MatrixObject<T> L(num_of_landmarks,3);
+MatrixObject<T> x_ref(3,1);
+
+for(Ti i = 0; i < num_of_landmarks;i++){
+   for(Ti j = 0; j < 3;j++){
+      L(i,j) = u_g(gen1);
+   }
+}
+
+for(Ti i = 0;i < 3;i++){
+   x_ref(i,0) = u_g(gen2);
+}
+
+return {L, x_ref};   
+}
+
+MatrixObject<T> distanceMetric(const MatrixObject<T>& L,const MatrixObject<T>& x)const{
 MatrixObject<T> dist_curr(L.getRows(),1);
 for(Ti i = 0;i < dist_curr.getRows();i++){
    T summation = 0.0;
    for(Ti j = 0;j < 3;j++){
-      summation += matcalc.square(L(i,j) - x(j,0));
+      summation += matcalc.square(x(j,0) - L(i,j));
    }
    dist_curr(i,0) = sqrt(summation);
 }
 return dist_curr;   
 }
 
-tuple<MatrixObject<T>,MatrixObject<T>> GenerateMatrixData_GN(const MatrixObject<T>& L, const MatrixObject<T> d_meas,const MatrixObject<T>& d_curr, MatrixObject<T>& x){
+tuple<MatrixObject<T>,MatrixObject<T>> GenerateMatrixData_GN(const MatrixObject<T>& L, const MatrixObject<T> d_truth,const MatrixObject<T>& d_meas, MatrixObject<T>& x)const{
 MatrixObject<T> A(L.getRows(),3);
 MatrixObject<T> b(L.getRows(),1);
-for(Ti i = 0;i < A.getRows();i++){
+
+for(Ti i = 0;i < L.getRows();i++){
    for(Ti j = 0;j < 3;j++){
-      A(i,j) = -(L(i,j) - x(j,0))/d_curr(i,0);
+      A(i,j) = (x(j,0)-L(i,j))/d_meas(i,0);
    }
 }
-
-for(Ti i = 0;i < b.getRows();i++){
-   b(i,0) = d_meas(i,0) - d_curr(i,0);
+for(Ti i = 0;i < L.getRows();i++){
+   b(i,0) = d_truth(i,0) - d_meas(i,0); 
 }
 return {A,b};
 }
 
-tuple<LinkedListObject<T>, LinkedListObject<T>, LinkedListObject<MatrixObject<T>>> TrackingMission_SingleTargetUAV(const MatrixObject<T>& L, const MatrixObject<T>& dist_meas, MatrixObject<T>& x, const Ti& time_limit, const T& eps){
+tuple<LinkedListObject<T>, LinkedListObject<T>, LinkedListObject<MatrixObject<T>>> TrackingMission_SingleTargetUAV(const MatrixObject<T>& L, const MatrixObject<T>& dist_truth,MatrixObject<T>& x, const T& eps)const{
 LinkedListObject<T> time_list;
 LinkedListObject<T> L2error_list;
 LinkedListObject<MatrixObject<T>> x_list;
 x_list.addElement(x);
 cout << "\nMission started. Tracking down the sensor coordinates for the single-targeted UAV starting...\n";
-for(Ti i = 0;i < time_limit;i++){
-   cout << "\nTime elapsed: " << i << " seconds...\n";
-   time_list.addElement(i);
-   MatrixObject<T> d_curr = distanceMetric(L,x);
-   auto [A,b] = GenerateMatrixData_GN(L,dist_meas,d_curr,x);
+T delta_time = 0.0;
+bool stillSearching = true;
+while(stillSearching){
+   cout << "\nTime elapsed: " << delta_time << " seconds...\n";
+   time_list.addElement(delta_time);
+   MatrixObject<T> d_meas = distanceMetric(L,x);
+   auto [A,b] = GenerateMatrixData_GN(L,dist_truth,d_meas,x);
    MatrixObject<T> x_L2 = matcalc.LeastSquareSolver_QRDecomposition(A,b);
+   cout << "\n Direction vector: \n";
+   x_L2.print();
    T norm_val = matcalc.L2Norm(x_L2);
    cout << "\nNorm: \n"<< norm_val;
    L2error_list.addElement(norm_val);
+   x = x + x_L2;
    x_list.addElement(x);
-   MatrixObject<T> x_new = x_L2.scalarMatrix(0.8);
-   x = x + x_new;
    if(abs(norm_val) < eps){
+      stillSearching = false;
      cout << "\nUAV found!\n";
      cout << "\nSensor coordinates: \n";
      x.print(); 
      break;
    }
    cout << "\nUpdated x\n";
-   if(time_limit - i == 1){
+   x.print();
+   if(delta_time == 3600){
      cout << "\nMission failed. Reason: The UAV couldn't be tracked down in time.\n"; 
      cout << "\nThe sensor system found the closest coordinates: \n";
      const MatrixObject<T>& x_est = x_list.getLastElement();
      x_est.print();
    }
+   delta_time += 1.0;
 }
 
 return {time_list,L2error_list, x_list};   
 }
 
-void UAVTracking_Solver(const Ti& num_of_uavs, const MatrixObject<T>& L, const MatrixObject<T>& dist_meas, MatrixObject<T>& x, const Ti& time_limit){
+void UAVTracking_Solver(const Ti& num_of_uavs, const MatrixObject<T>& L, const MatrixObject<T>& dist_truth, MatrixObject<T>& uav_guess)const{
 const T eps = 1e-4;
-cout << "Welcome to the UAV tracking program. You can use the sensor system to track down any number of UAVs you'd like.\n"; 
-cout << "You have chosen a number of " << num_of_uavs <<  " UAVs.\n";
+cout << "\nWelcome to the UAV tracking program. You can use the sensor system to track down any number of UAVs you'd like.\n"; 
+cout << "\nYou have chosen a number of " << num_of_uavs <<  " UAVs.\n";
 if(num_of_uavs < 2){
-   auto [time_list,L2error_list,x_list] = TrackingMission_SingleTargetUAV(L, dist_meas, x, time_limit,eps);
+   auto [time_list,L2error_list,x_list] = TrackingMission_SingleTargetUAV(L, dist_truth, uav_guess,eps);
    cout << "\nTime: " << time_list.getLastElement() << " seconds.\n";
    cout << "\nL2 error: " << L2error_list.getLastElement() << "\n";
    cout << "\nSensor coordinates for the UAV: \n";
    const MatrixObject<T>& x_est = x_list.getLastElement();
    x_est.print(); 
+   MatrixObject<T> d_xest = distanceMetric(L,x_est);
+   cout << "\nEstimated solution, distances\n";
+   d_xest.print();
 }
 else{
    throw invalid_argument("Reason: The UAV tracking program isn't developed for tracking down several UAVs yet. Such an update will arrive in the future.");
--- EmbeddedDroneTracking_project/test_droneTracking.cpp
+++ EmbeddedDroneTracking_project/test_droneTracking.cpp
@@ -10,71 +10,35 @@ UAVTracking <double,int> uav_tracking;
 
 using namespace std;
 
-TEST(TrackUAVs,MultipleUAVs){
-   int numofuavs = 7;
-   MatrixObject<double> L(4,3);
-   MatrixObject<double> dist_meas(4,1);
-   MatrixObject<double> uav_sensors(3,1);
-   int time_limit = 21600;
-   tuple<MatrixObject<double>,MatrixObject<double>> landmarkParams {L,dist_meas};
-
-   L(0,0) = 1.0;
-   L(0,1) = 2.0;
-   L(0,2) = 3.0;
-   L(1,0) = 4.0;
-   L(1,1) = -8.0;
-   L(1,2) = -9.0;
-   L(2,0) = 1.0;
-   L(2,1) = -1.0;
-   L(2,2) = 1.0;
-   L(3,0) = 1.0;
-   L(3,1) = -1.0;
-   L(3,2) = 3.0;
-
-   dist_meas(0,0) = 0.5;
-   dist_meas(1,0) = 1.0;
-   dist_meas(2,0) = 2.0;
-   dist_meas(3,0) = 1.0;
-
-   uav_sensors(0,0) = -3.0;
-   uav_sensors(1,0) = -2.0;
-   uav_sensors(1,0) = 1.0;
-
-   uav_tracking.UAVTracking_Solver(numofuavs,L,dist_meas,uav_sensors,time_limit);
-}
-
 TEST(TrackUAVs,OneUAV){
    int numofuavs = 1;
-   MatrixObject<double> L(3,3);
-   MatrixObject<double> dist_meas(4,1);
-   MatrixObject<double> uav_sensors(3,1);
+   int numoflms = 30;
+
+   tuple<double,double> axis_range{-10,10};
+   int l_seed = 1;
+   int ref_seed = 5;
+   tuple<int,int> seeds{l_seed,ref_seed};
+
    MatrixObject<double> uav_guess(3,1); 
-   int time_limit = 1000000;
-   tuple<MatrixObject<double>,MatrixObject<double>> landmarkParams {L,dist_meas};
 
-   L(0,0) = 1.0;
-   L(0,1) = 0.0;
-   L(0,2) = 0.0;
-   L(1,0) = -1.0;
-   L(1,1) = 1.0;
-   L(1,2) = 1.0;
-   L(2,0) = 0.0;
-   L(2,1) = 0.0;
-   L(2,2) = 1.0;
+   auto [L,uav_ref] = uav_tracking.generateRadar(numoflms,axis_range,seeds);
 
-   uav_guess(0,0) = 1.0;
-   uav_guess(1,0) = 0.0;
-   uav_guess(2,0) = 1.0;
+   uav_guess(0,0) = -10000.0;
+   uav_guess(1,0) = -120000.0;
+   uav_guess(2,0) = -103200.0;
 
-   uav_sensors(0,0) = 1.0;
-   uav_sensors(1,0) = 0.0;
-   uav_sensors(2,0) = 0.0;
-
-   MatrixObject<double> d_meas = uav_tracking.distanceMetric(L,uav_sensors);
-   MatrixObject<double> d_init = uav_tracking.distanceMetric(L,uav_guess);
-   cout << "\n Distances, Ground Truth locations: \n";
+   cout << "\nLandmarks, locations\n";
+   L.print();
+   cout << "\nReference point, locations\n";
+   uav_ref.print();
+   MatrixObject<double> d_truth = uav_tracking.distanceMetric(L,uav_ref);
+   cout << "\nReference point, distances to landmarks\n";
+   d_truth.print();  
+   cout << "\nUAV, Initial guess\n";
+   uav_guess.print();
+   MatrixObject<double> d_meas = uav_tracking.distanceMetric(L,uav_guess);
+   cout << "\nEstimated distances\n";
    d_meas.print();
-   cout << "\nDistances, Initial guess: \n";
-   d_init.print();
-   uav_tracking.UAVTracking_Solver(numofuavs,L,dist_meas,uav_guess,time_limit);
+
+   uav_tracking.UAVTracking_Solver(numofuavs,L,d_truth,uav_guess);
 }
